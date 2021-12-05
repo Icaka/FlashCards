b8a15aff7e0f9790c3d9a4bc4a59ae6bf78f924f
Icaka <ico.ezekiev@gmail.com>
2021-12-05 14:53:33 +0200

    UserRepository

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from user import User
+from user_repository import UserRepository
 
 if __name__ == '__main__':
     #print_hi('PyCharm')
@@ -11,6 +12,13 @@ if __name__ == '__main__':
     # print(data['users']['5'])
     # print(data['users']['5']['fName'])
     u1 = User()
+    u2 = User()
     print(u1)
     u1.create_from_dict(data['users']['5'])
+    u2.create_from_dict((data['users']['10']))
     print(u1)
+    repo = UserRepository()
+    repo.insert(u1)
+    repo.insert(u2)
+    print(repo.find_by_id(5))
+    print(repo.find_by_id(10))
---
+++ user_repository.py
@@ -0,0 +1,30 @@
+class UserRepository:
+    next_id = 0
+
+    @classmethod
+    def get_next_id(cls):
+        cls.next_id += 1
+        return cls.next_id
+
+    def __init__(self, users = {}, ):
+        self.users = users
+
+    def find_by_id(self, id):
+        return self.users[id]
+
+    def insert(self, user):
+        # user.id = self.__class__.get_next_id()
+        self.users[user.id] = user
+        return user
+
+    def update(self, user):
+        self.users[user.id] = user
+        return user
+
+    def delete_by_id(self, id):
+        removed = self.users[id]
+        del self.users[id]
+        return removed
+
+    def count(self):
+        return len(self.users)
